25aff0b58c981003dd43aeff9737cd98ff526994
Conan Tzou <conantzou@gmail.com>
2015-04-25 23:50:13 -0700

    refactor my_transpose

--- my_transpose.rb
+++ my_transpose.rb
@@ -1,15 +1,19 @@
 class Array
   def my_transpose
-    transposed = []
-    (0..self.length - 1).each do |col_idx|
-      sub_array = []
-      (0..self.length - 1).each do |row_idx|
-        sub_array << self[row_idx][col_idx]
+    transposed = Array.new(length) { Array.new(length) }
+
+    each_index do |y|
+      each_index do |x|
+        transposed[y][x] = self[x][y]
       end
-      transposed << sub_array
     end
+
     transposed
   end
 end
 
-p [[0, 1, 2],[3, 4, 5],[6, 7, 8]].my_transpose == [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+p [[0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8]].my_transpose == [[0, 3, 6], 
+                               [1, 4, 7], 
+                               [2, 5, 8]]

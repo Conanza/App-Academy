ba820691c74fb32b3ea2ba73b62288e201c12a32
Conan Tzou <conantzou@gmail.com>
2015-04-26 01:28:03 -0700

    refactor num_to_s

--- num_to_s.rb
+++ num_to_s.rb
@@ -1,37 +1,43 @@
+CONVERSIONS = { 
+  0 => "0",
+  1 => "1",
+  2 => "2",
+  3 => "3",
+  4 => "4",
+  5 => "5",
+  6 => "6",
+  7 => "7",
+  8 => "8",
+  9 => "9",
+ 10 => "A",
+ 11 => "B",
+ 12 => "C",
+ 13 => "D",
+ 14 => "E",
+ 15 => "F"
+}
+
 def num_to_s(num, base)
-  units = Hash[
-    0, "0",
-    1, "1",
-    2, "2",
-    3, "3",
-    4, "4",
-    5, "5",
-    6, "6",
-    7, "7",
-    8, "8",
-    9, "9",
-   10, "A",
-   11, "B",
-   12, "C",
-   13, "D",
-   14, "E",
-   15,"F"]
+  new_num = ""
+  
+  power = 0
+  while base ** power < num 
+    value = base ** power
+    unit = (num / value) % base
 
-  i = 0
-  result = ""
-  while base ** i < num
-    result << units[(num / (base ** i)) % base]
-
-    i += 1
+    new_num << CONVERSIONS[unit]
+    power += 1
   end
-  result.reverse
+
+  new_num.reverse
 end
 
+if $PROGRAM_NAME == __FILE__
+  p num_to_s(5, 10) == "5"
+  p num_to_s(5, 2)  == "101"
+  p num_to_s(5, 16) == "5"
 
-p num_to_s(5, 10) == "5"
-p num_to_s(5, 2)  == "101"
-p num_to_s(5, 16) == "5"
-
-p num_to_s(234, 10) == "234"
-p num_to_s(234, 2)  == "11101010"
-p num_to_s(234, 16) == "EA"
+  p num_to_s(234, 10) == "234"
+  p num_to_s(234, 2)  == "11101010"
+  p num_to_s(234, 16) == "EA"
+end

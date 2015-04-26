13bac5285207fbf5f2c723121c3199bd79c694c6
Conan Tzou <conantzou@gmail.com>
2015-04-26 02:57:07 -0700

    refactor iteration exercises

--- iteration.rb
+++ iteration.rb
@@ -1,19 +1,12 @@
+require 'set'
+
 def factors(num)
   (1..num).select { |div| num % div == 0 }
 end
 
-# def bubblesort(arr)
-#   (arr.length - 1).times do
-#     (arr.length - 1).times do |i|
-#       arr[i], arr[i+1] = arr[i+1], arr[i] if arr[i] > arr[i+1]
-#     end
-#   end
-#   arr
-# end
-
 class Array
   def bubble_sort
-    self.dup.bubble_sort!
+    dup.bubble_sort!
   end
 
   def bubble_sort!
@@ -21,25 +14,25 @@ class Array
 
     until sorted 
       sorted = true
-      (1...self.length).each do |idx|
+      (1...length).each do |idx|
         prev_num, curr_num = self[idx - 1], self[idx]
         
         if prev_num > curr_num
-          sorted = false
-          self[idx - 1], self[idx] = curr_num, prev_num 
+          sorted, self[idx - 1], self[idx] = false, curr_num, prev_num 
         end
       end
     end
+
     self
   end
 end
 
-def substring(str)
+def substring(string)
   substrings = []
 
-  str.each_char.with_index do |char, idx|
-    (1..str.length - idx).each do |length|
-      substring = str[idx, length]
+  string.length.times do |idx|
+    (1..string.length - idx).each do |length|
+      substring = string[idx, length]
 
       substrings << substring unless substrings.include?(substring)
     end
@@ -48,13 +41,14 @@ def substring(str)
   substrings
 end
 
-def subwords(str)
-  substrs = substring(str)
-  dict = File.readlines("dictionary.txt")
-  substrs.select { |substr| dict.include?("#{substr}\n") }.map(&:chomp)
+def subwords(string)
+  substrings = substring(string)
+  dictionary = Set.new(File.readlines("dictionary.txt").map(&:chomp))
+  substrings.select { |subs| dictionary.include?(subs) }
 end
 
-
-p [4, 6, 3, 2, 1].bubble_sort
-p substring("cat")
-p subwords("cat")
+if $PROGRAM_NAME == __FILE__
+  p [4, 6, 3, 2, 1].bubble_sort
+  p substring("cat")
+  p subwords("cat")
+end

9fd201f41cc5c06ff8dd435bc9656babadc2e3dd
Conan Tzou <conantzou@gmail.com>
2015-04-26 02:08:33 -0700

    refactor enumerables

--- enumerables.rb
+++ enumerables.rb
@@ -1,49 +1,54 @@
-
-def times_two(array)
-  array.map(&:double)
-end
-
 class Integer
   def double
-    self*2
+    self * 2
   end
 end
 
 class Array
+  def avg
+    reduce(:+) / length.to_f
+  end
+
+  def median
+    sorted = self.sort
+    mid = length / 2
+    length.odd? ? sorted[mid] : [sorted[mid], sorted[mid - 1]].avg
+  end
+
   def my_each(&block)
-    self.length.times do |index|
+    length.times do |index|
       block.call(self[index])
     end
+    
     self
+  end
+
+  def string_concat
+    reduce(:+)    
+  end
+  
+  def times_two
+    map(&:double)
   end
 end
 
-def median(arr)
-  sorted_nums = arr.sort
-  midpoint = arr.length / 2
-  arr.length.odd? ? sorted_nums[midpoint] : (sorted_nums[midpoint - 1] + sorted_nums[midpoint]) / 2.to_f
+if $PROGRAM_NAME == __FILE__
+  puts "times_two test"
+  puts [1, 2, 3, 4].times_two == [2, 4, 6, 8]
+
+  p "my_each test"
+  return_value = [1, 2, 3]
+  return_value.my_each do |num|
+    puts num
+  end.my_each do |num|
+    puts num
+  end
+  p return_value == [1, 2, 3]
+
+  p "median tests"
+  p [1,2,3].median == 2
+  p [1,2,3,4].median == 2.5
+
+  p "concat test"
+  p ["Yay ", "for ", "strings!"].string_concat == "Yay for strings!"
 end
-
-def string_cats(array)
-  array.inject(:+)
-end
-
-puts "times_two test"
-puts times_two([1, 2, 3, 4]) == [2, 4, 6, 8]
-
-p "my_each test"
-return_value = [1, 2, 3]
-return_value.my_each do |num|
-  puts num
-end.my_each do |num|
-  puts num
-end
-p return_value == [1, 2, 3]
-
-p "median tests"
-p median([1,2,3]) == 2
-p median([1,2,3,4]) == 2.5
-
-p "concat test"
-p string_cats(["Yay ", "for ", "strings!"]) == "Yay for strings!"
-

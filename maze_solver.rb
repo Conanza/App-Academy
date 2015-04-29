2a997df2ff4b13b6154853901709c430f3da4afb
Conan Tzou <conantzou@gmail.com>
2015-04-29 01:49:34 -0700

    Add maze solver and its instructions to file

---
+++ maze_solver.rb
@@ -0,0 +1,41 @@
+# Write a program that will find a route between two points in a maze
+#
+# ****************
+# *         *   E*
+# *    *    *  ***
+# *    *    *    *
+# *    *    *    *
+# *    *    *    *
+# *S   *         *
+# ****************
+#
+# S being start and E being end 
+#
+# Read in the maze, try to explore through it to the end, 
+# and print out a completed path
+#
+# ****************
+# *XXXXXXXXX* XXE*
+# *X   *   X* X***
+# *X   *   X* X  *
+# *X   *   X* X  *
+# *X   *   X* X  *
+# *S   *   XXXX  *
+# ****************
+#
+# run program as a cmd line script, taking in maze file on the cmd line
+#
+# path through maze shouldn't be self intersecting
+#
+# after find a solution, write a version that guarantees the shortest path
+
+
+#!/usr/bin/env ruby
+
+class MazeSolver
+
+end
+
+if $PROGRAM_NAME == __FILE__
+  
+end

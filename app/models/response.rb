c8b49dfbf7fd3606486fff62ea419270a1fdc4a1
Conan Tzou <conantzou@gmail.com>
2015-05-07 18:46:09 -0700

    Add custom response validations

--- app/models/response.rb
+++ app/models/response.rb
@@ -11,6 +11,8 @@
 
 class Response < ActiveRecord::Base
   validates :respondent_id, :answer_choice_id, presence: true
+  validate :respondent_has_not_already_answered_question
+  validate :respondent_is_not_poll_author
 
   belongs_to(
     :respondent,
@@ -28,11 +30,28 @@ class Response < ActiveRecord::Base
 
   has_one :question, through: :answer_choice, source: :question
 
+  # pull all responses for a question for 2 cases:
+  # 1. if current response isn't saved yet, self.id will be null
+  #    thus, pull everything self.question.responses returns
+  #  OR 
+  # 2. if curr response is saved,
+  #    then find all responses where responses.id is not same as self.id
   def sibling_responses
-    self.question.responses # returns all response objects for this response's question
+    self.question.responses
+      .where('? IS NULL OR responses.id != ?', self.id, self.id)
   end
 
   def respondent_has_not_already_answered_question
+    if sibling_responses.exists?(respondent_id: self.respondent_id)
+      errors[:respondent_id] << "has already answered this question"
+    end
+  end
 
+  def respondent_is_not_poll_author
+    poll_author = self.question.poll.author_id
+
+    if poll_author == self.respondent_id
+      errors[:respondent_id] << "cannot be the poll's author"
+    end
   end
 end

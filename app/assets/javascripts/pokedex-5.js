c32c9faaf34b2b71513a6138740800f25d869185
Conan Tzou <conantzou@gmail.com>
2015-06-02 14:03:16 -0700

    Begin routing for pokemon index & details

--- app/assets/javascripts/pokedex-5.js
+++ app/assets/javascripts/pokedex-5.js
@@ -2,47 +2,90 @@ Pokedex.Views = {}
 
 Pokedex.Views.PokemonIndex = Backbone.View.extend({
   events: {
+    "click li": "selectPokemonFromList"
   },
 
   initialize: function () {
+    this.listenTo(this.collection, "sync", this.render);
+    this.listenTo(this.collection, "add", this.addPokemonToList);
   },
 
   addPokemonToList: function (pokemon) {
+    this.$el.append(JST["pokemonListItem"]({pokemon: pokemon}));
   },
 
-  refreshPokemon: function (options) {
+  refreshPokemon: function (options, callback) {
+    var that = this;
+    this.collection.fetch( {
+      success: function () {
+        that.render();
+        callback();
+      }
+    }); // do a empty & add to list callback on success?
   },
 
   render: function () {
+    this.$el.empty();
+    this.collection.each(function(pokemon) {
+      this.addPokemonToList(pokemon);
+    }.bind(this));
+    return this;
   },
 
   selectPokemonFromList: function (event) {
+    var id = $(event.currentTarget).data("id");
+
+    Backbone.history.navigate("pokemon/" + id, { trigger: true });
+    // var poke = this.collection.get(id);
+    // this.detailView && this.detailView.remove();
+    // this.detailView = new Pokedex.Views.PokemonDetail({
+    //   model: poke,
+    // });
+    // $("#pokedex .pokemon-detail").html(this.detailView.render().$el)
+    // this.detailView.refreshPokemon();
   }
 });
 
 Pokedex.Views.PokemonDetail = Backbone.View.extend({
   events: {
+    "click .toys li": "selectToyFromList"
   },
 
   refreshPokemon: function (options) {
+    var that = this;
+    this.model.fetch({
+      success: function () {
+        that.render();
+      }
+    });
   },
 
   render: function () {
+    this.$el.html(JST["pokemonDetail"]({ pokemon: this.model }));
+    var $toys = $('<ul class="toys"></ul>');
+    this.$el.append($toys);
+
+    this.model.toys().each(function (toy) {
+      $toys.append(JST["toyListItem"]({ toy: toy }));
+    });
+    return this;
   },
 
   selectToyFromList: function (event) {
+    var toy = this.model.toys().get($(event.currentTarget).data("id"))
+
+    this.toyDetailView && this.toyDetailView.remove();
+    this.toyDetailView = new Pokedex.Views.ToyDetail({
+      model: toy
+    });
+
+    $("#pokedex .toy-detail").html(this.toyDetailView.render().$el)
   }
 });
 
 Pokedex.Views.ToyDetail = Backbone.View.extend({
   render: function () {
+    this.$el.html(JST["toyDetail"]({ toy: this.model, pokes: new Pokedex.Collections.Pokemon() }));
+    return this;
   }
 });
-
-/*
-$(function () {
-  var pokemonIndex = new Pokedex.Views.PokemonIndex();
-  pokemonIndex.refreshPokemon();
-  $("#pokedex .pokemon-list").html(pokemonIndex.$el);
-});
-*/
--- app/assets/javascripts/pokedex-6.js
+++ app/assets/javascripts/pokedex-6.js
@@ -1,23 +1,61 @@
 Pokedex.Router = Backbone.Router.extend({
   routes: {
+    "": "pokemonIndex",
+    "pokemon/:id": "pokemonDetail"
   },
 
   pokemonDetail: function (id, callback) {
+    if (!this._pokemonIndex) {
+      this.pokemonIndex(this.pokemonDetail.bind(this, id));
+    } else {
+      var collection = this._pokemonIndex.collection;
+      var poke = collection.get(id);
+      this.detailView = new Pokedex.Views.PokemonDetail({
+        model: poke,
+      });
+        this.detailView.refreshPokemon();
+
+      this._swapView(this.detailView);
+
+      $("#pokedex .pokemon-detail").html(this.detailView.$el);
+    }
   },
 
   pokemonIndex: function (callback) {
+    var $el = $("#pokedex .pokemon-list");
+    var pokes = new Pokedex.Collections.Pokemon();
+    var pokemonIndexView = new Pokedex.Views.PokemonIndex({
+      collection: pokes,
+      el: $el
+    });
+
+    this._pokemonIndex = pokemonIndexView.render();
+    pokes.fetch({
+      success: function () {
+        callback();
+      }
+    });
   },
 
   toyDetail: function (pokemonId, toyId) {
   },
 
   pokemonForm: function () {
+  },
+
+  _swapView: function (newView) {
+    if (this._currentView) {
+      this._currentView.remove();
+    }
+
+    this._currentView = newView;
+    newView.render();
   }
 });
 
-/*
+
 $(function () {
   new Pokedex.Router();
   Backbone.history.start();
+
 });
-*/

b41023d9b75be3a49c1f3543ba00f6ea9da78d2f
Conan Tzou <conantzou@gmail.com>
2015-06-25 10:15:13 -0700

    answer bonus questions

--- skeleton/lib/03a_nobel_bonus.rb
+++ skeleton/lib/03a_nobel_bonus.rb
@@ -13,6 +13,37 @@ require_relative './sqlzoo.rb'
 
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
+    # SELECT DISTINCT
+    #   nobels.yr
+    # FROM 
+    #   nobels
+    # LEFT OUTER JOIN 
+    #   (
+    #     SELECT
+    #       yr
+    #     FROM
+    #       nobels
+    #     WHERE
+    #       subject = 'Chemistry'
+    #   ) AS chemistry_years ON nobels.yr = chemistry_years.yr
+    # WHERE
+    #   subject = 'Physics' AND chemistry_years.yr IS NULL
   execute(<<-SQL)
+
+
+    SELECT DISTINCT
+      yr
+    FROM 
+      nobels
+    WHERE
+      subject = 'Physics' AND yr NOT IN (
+        SELECT
+          yr
+        FROM 
+          nobels
+        WHERE
+          subject = 'Chemistry'
+        )
+
   SQL
 end
--- skeleton/lib/04a_countries_bonus.rb
+++ skeleton/lib/04a_countries_bonus.rb
@@ -17,13 +17,56 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      name 
+    FROM
+      countries
+    WHERE
+      gdp > (
+        SELECT
+          MAX(gdp)
+        FROM 
+          countries
+        WHERE
+          continent = 'Europe'
+      )
+
   SQL
 end
 
 def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
+    # SELECT
+    #   c1.continent, c1.name, c1.area
+    # FROM
+    #   countries c1
+    # WHERE
+    #   c1.area = (
+    #     SELECT
+    #       MAX(c2.area)
+    #     FROM
+    #       countries c2
+    #     WHERE
+    #       c1.continent = c2.continent
+    #   )
   execute(<<-SQL)
+    SELECT
+      c1.continent, c1.name, c1.area
+    FROM 
+      countries c1
+    INNER JOIN
+      (
+        SELECT
+          continent, MAX(area) AS area
+        FROM
+          countries
+        GROUP BY 
+          continent
+      ) AS c2 ON c2.continent = c1.continent
+    WHERE
+      c1.area = c2.area
+
   SQL
 end
 
@@ -31,5 +74,19 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT 
+      c1.name, c1.continent
+    FROM 
+      countries c1
+    WHERE
+      population > 3 * (
+        SELECT
+          MAX(c2.population)
+        FROM
+          countries c2
+        WHERE
+          c1.name != c2.name AND c1.continent = c2.continent
+      )
+
   SQL
 end

dd1a5c17baefb8a8d64638ae1ae3c89e6b5efd54
Conan Tzou <conantzou@gmail.com>
2015-07-31 21:22:43 -0700

    add ringbuffer

--- data-structures/lib/ring_buffer.rb
+++ data-structures/lib/ring_buffer.rb
@@ -1,6 +1,81 @@
 class RingBuffer
-  def initialize
+  attr_reader :length
 
+  def initialize(n)
+    @store = Array.new(n, nil)
+    @capacity, @length, @start_idx = n, 0, 0
   end
-  
+
+  # O(1)
+  def [](index)
+    check_index(index)
+    @store[(start_idx + index) % capacity]
+  end
+
+  # O(1)
+  def []=(index, val)
+    check_index(index)
+    @store[(start_idx + index) % capacity] = val
+  end
+
+  # O(1)
+  def pop
+    return nil unless length > 0
+    val, self[length - 1] = self[length - 1], nil
+    @length -= 1
+
+    val
+  end
+
+  # O(1) amortized
+  # O(n) W.C.
+  def push(value)
+    resize! if length == capacity
+
+    @length += 1
+    self[length - 1] = value
+
+    self
+  end
+
+  # O(1)
+  def shift
+    return nil unless length > 0
+    val, self[0] = self[0], nil
+    @length -= 1
+    @start_idx = (@start_idx + 1) % capacity
+
+    val
+  end
+
+  # O(1) amortized
+  # O(n) W.C.
+  def unshift(value)
+    resize! if length == capacity
+    @start_idx = (@start_idx - 1) % capacity
+    self[0] = value
+    @length += 1
+
+    self
+  end
+
+  private
+
+  attr_reader :capacity, :start_idx, :store
+
+  def check_index(index)
+    unless index >= 0 && index < length
+      raise "index out of bounds"
+    end
+  end
+
+  def resize!
+    new_capacity = capacity * 2
+    new_store = Array.new(new_capacity, nil)
+    length.times { |i| new_store[i] = self[i] }
+
+    @capacity = new_capacity
+    @store = new_store
+    @start_idx = 0
+  end
 end
---
+++ data-structures/specs/ring_buffer_spec.rb
@@ -0,0 +1,103 @@
+require "ring_buffer"
+
+describe RingBuffer do
+  before(:each) do
+    @ringbuffer = RingBuffer.new(4)
+  end
+
+  context "when initialized" do
+    it "starts out empty" do
+      expect(@ringbuffer.length).to be(0)
+    end
+
+    it "raises an error when indexing into the ring buffer" do
+      expect { @ringbuffer[1] }.to raise_error "index out of bounds"
+    end
+
+    it "has the right capacity" do
+      expect(@ringbuffer.send(:capacity)).to be(4)
+    end
+
+    it "begins at index 0" do
+      expect(@ringbuffer.send(:start_idx)).to be(0)
+    end
+  end
+
+  context "when pushing and popping" do
+    before(:each) do
+      4.times { |i| @ringbuffer.push(i) }
+    end
+
+    it "has the right length" do
+      expect(@ringbuffer.length).to be(4)
+    end
+
+    it "still has start idx of 0" do
+      expect(@ringbuffer.send(:start_idx)).to be(0)
+    end
+
+    it "returns the right value" do
+      expect(@ringbuffer.pop).to be(3)
+    end
+  end
+
+  context "when unshifting and shifting" do
+    before(:each) do
+      3.times { |i| @ringbuffer.push(i) }
+      @ringbuffer.unshift(5)
+    end
+
+    it "has the right length" do
+      expect(@ringbuffer.length).to be(4)
+    end
+
+    it "changes the start_idx" do
+      expect(@ringbuffer.send(:start_idx)).to be(3)
+    end
+
+    it "retrieves the right value" do
+      expect(@ringbuffer[0]).to be(5)
+    end
+
+    it "shifts off the right value" do
+      expect(@ringbuffer.shift).to be(5)
+    end
+
+    it "changes the start_idx correctly after shifting" do
+      @ringbuffer.shift
+      expect(@ringbuffer.send(:start_idx)).to be(0)
+    end
+  end
+
+  context "after resizing" do
+    before(:each) do
+      4.times { |i| @ringbuffer.push(i) }
+      @ringbuffer.unshift(5)
+    end
+
+    it "increases capacity" do
+      expect(@ringbuffer.send(:capacity)).to be(8)
+    end
+
+    it "has the right length" do
+      expect(@ringbuffer.length).to be(5)
+    end
+
+    it "has the right start_idx" do
+      expect(@ringbuffer.send(:start_idx)).to be(7)
+    end
+
+    it "pops off the right value" do
+      expect(@ringbuffer.pop).to be(3)
+    end
+
+    it "shifts off the right value" do
+      expect(@ringbuffer.shift).to be(5)
+    end
+
+    it "changes start_idx correctly after a shift" do
+      @ringbuffer.shift
+      expect(@ringbuffer.send(:start_idx)).to be(0)
+    end
+  end
+end
